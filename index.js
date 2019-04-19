37000746152a54ef408d783ae3da48e94bba024e
Abby Chao <abigail.chao@gmail.com>
2019-04-19 02:23:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ function theBeatlesPlay (musicians, instruments) {
   var array = []
   for (i = 0; i<musicians.length; i++) {
     var string = `${musicians[0]} plays ${instruments[0]}`
+    array.push(string)
+    return array
   }
+}
+
+function johnLennonFacts(facts) {
+  //
 }

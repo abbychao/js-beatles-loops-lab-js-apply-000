b0904b5d7e1166957d024f38f3ee1c83aae223b1
Abby Chao <abigail.chao@gmail.com>
2019-04-19 02:30:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // add solution here
 function theBeatlesPlay (musicians, instruments) {
-  var array = []
+  var result = []
   var i 
   for (i = 0; i<musicians.length; i++) {
     var string = `${musicians[i]} plays ${instruments[i]}`
-    array.push(string)
+    result.push(string)
   }
-  return array
+  return result
 }
 
 function johnLennonFacts(facts) {
   var i 
   var result = []
   while(i<facts.length) {
-    result[i].push(facts[i] + "!!!")
+    result.push(facts[i] + "!!!")
     i++
   }
   return result 

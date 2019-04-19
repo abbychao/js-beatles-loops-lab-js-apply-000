98e90ceb67c73e58003b29abf4dc0d4031e5b1ba
Abby Chao <abigail.chao@gmail.com>
2019-04-19 02:22:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // add solution here
 function theBeatlesPlay (musicians, instruments) {
   var array = []
-  for (i = 0; i<musicians.length; i++) 
+  for (i = 0; i<musicians.length; i++) {
+    var string = `${musicians[0]} plays ${instruments[0]}`
+  }
 }
